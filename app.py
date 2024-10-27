e5700562c3fc7c709462ff4616b45a81f10000ed
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 11:55:16 +0100

    Refactor imports and add authors blueprint and resource for handling author-related API endpoints

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from flask import Flask
 from flask_restful import Api
-from ressources.book_ressource import BookResource
+from resources.book_resource import BookResource
+from resources.author_resource import AuthorResource
 from models.db import db
 from schemas.schemas import ma
 from blueprints.books_bp import books_bp
+from blueprints.authors_bp import authors_bp
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db.init_app(app)
 ma.init_app(app)
-api = Api(app)
 
 with app.app_context():
     db.create_all()
 
-
-api.add_resource(BookResource, '/books', '/books/<int:book_id>')
-
-
 app.register_blueprint(books_bp, url_prefix='/books')
+app.register_blueprint(authors_bp, url_prefix='/authors')
 
 if __name__ == "__main__":
     app.run(debug=True)
