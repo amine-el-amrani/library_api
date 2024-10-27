c54cc63a61b1dfde35e3a3b578ffcfaecdd88b6c
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 12:21:38 +0100

    Refactor authors_bp.py to use Flask-RESTX and add Namespace for handling author-related API endpoints

--- blueprints/authors_bp.py
+++ blueprints/authors_bp.py
@@ -1,8 +1,17 @@
 from flask import Blueprint
-from flask_restful import Api
-from resources.author_resource import AuthorResource
+from flask_restx import Api, Namespace
+from resources.author_resource import AuthorListResource, AuthorResource
 
+# Créer le Blueprint pour les auteurs
 authors_bp = Blueprint('authors', __name__)
 api = Api(authors_bp)
 
-api.add_resource(AuthorResource, '', '/<int:author_id>')
+# Définir un Namespace pour les opérations sur les auteurs
+authors_ns = Namespace('authors', description="Operations related to authors")
+
+# Ajouter les ressources au Namespace
+authors_ns.add_resource(AuthorListResource, '/')
+authors_ns.add_resource(AuthorResource, '/<int:author_id>')
+
+# Ajouter le Namespace à l'API du Blueprint
+api.add_namespace(authors_ns)
