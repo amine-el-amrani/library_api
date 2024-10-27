6d2edd499fc08ad2a8dbd12fcf765fff9c252a85
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 12:21:44 +0100

    Refactor books_bp.py to use Flask-RESTX and add Namespace for handling book-related API endpoints

--- blueprints/books_bp.py
+++ blueprints/books_bp.py
@@ -1,10 +1,16 @@
 from flask import Blueprint
-from flask_restful import Api
-from resources.book_resource import BookResource
+from flask_restx import Api, Namespace
+from resources.book_resource import BookListResource, BookResource
 
-# Créer le Blueprint pour les livres
 books_bp = Blueprint('books', __name__)
 api = Api(books_bp)
 
-# Ajouter les routes pour les livres
-api.add_resource(BookResource, '', '/<int:book_id>')
+# Créer le namespace pour les livres
+books_ns = Namespace('books', description="Operations related to books")
+
+# Ajouter les ressources au namespace
+books_ns.add_resource(BookListResource, '/')
+books_ns.add_resource(BookResource, '/<int:book_id>')
+
+# Ajouter le namespace à l'API du Blueprint
+api.add_namespace(books_ns)
