0d28a577c449056022e46f03327735d1160fe7c3
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 12:22:17 +0100

    Refactor app.py to use Flask-RESTX and add Namespaces for handling book and author related API endpoints

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask
-from flask_restful import Api
-from resources.book_resource import BookResource
-from resources.author_resource import AuthorResource
 from models.db import db
+from flask_restx import Api
 from schemas.schemas import ma
-from blueprints.books_bp import books_bp
-from blueprints.authors_bp import authors_bp
+from blueprints.books_bp import books_bp, books_ns
+from blueprints.authors_bp import authors_bp, authors_ns
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db.init_app(app)
 ma.init_app(app)
 
-with app.app_context():
-    db.create_all()
+api = Api(app, version="1.0", title="Library API", description="Documentation automatique de l'API", doc="/docs")
+
+api.add_namespace(books_ns, path='/books')
+api.add_namespace(authors_ns, path='/authors')
 
 app.register_blueprint(books_bp, url_prefix='/books')
 app.register_blueprint(authors_bp, url_prefix='/authors')
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
