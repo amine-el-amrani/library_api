dad3714366d4fc87a2ee163a035c3cde9f1739e9
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 12:22:02 +0100

    Refactor author_resource.py to use Flask-RESTX and add Namespace for handling author-related API endpoints

--- resources/author_resource.py
+++ resources/author_resource.py
@@ -1,35 +1,48 @@
-from flask_restful import Resource
+from flask_restx import Resource, fields
 from flask import request
 from models.author import Author, db
 from schemas.author_schema import AuthorSchema
 from marshmallow import ValidationError
 
+# Initialiser le schéma Marshmallow pour Author
 author_schema = AuthorSchema()
 authors_schema = AuthorSchema(many=True)
 
-class AuthorResource(Resource):
-    def get(self, author_id=None):
-        if author_id:
-            author = Author.query.get(author_id)
-            if not author:
-                return {"message": "Author not found"}, 404
-            return author_schema.dump(author), 200
-        else:
-            authors = Author.query.all()
-            return authors_schema.dump(authors), 200
+# Définir le modèle pour la documentation Swagger avec Flask-RESTX
+author_model = {
+    "name": fields.String(required=True, description="Name of the author"),
+    "bio": fields.String(description="Biography of the author")
+}
+
+class AuthorListResource(Resource):
+    def get(self):
+        """List all authors"""
+        authors = Author.query.all()
+        return authors_schema.dump(authors), 200
 
     def post(self):
+        """Create a new author"""
         try:
             data = author_schema.load(request.get_json())
         except ValidationError as err:
             return {"errors": err.messages}, 400
 
-        new_author = data if isinstance(data, Author) else Author(**data)
+        new_author = Author(**data)
         db.session.add(new_author)
         db.session.commit()
         return {"message": "Author created", "author_id": new_author.id}, 201
 
+
+class AuthorResource(Resource):
+    def get(self, author_id):
+        """Retrieve an author by ID"""
+        author = Author.query.get(author_id)
+        if not author:
+            return {"message": "Author not found"}, 404
+        return author_schema.dump(author), 200
+
     def put(self, author_id):
+        """Update an author by ID"""
         author = Author.query.get(author_id)
         if not author:
             return {"message": "Author not found"}, 404
@@ -39,6 +52,7 @@ class AuthorResource(Resource):
         except ValidationError as err:
             return {"errors": err.messages}, 400
 
+        # Mettre à jour les champs de l'auteur
         for key, value in data.items():
             setattr(author, key, value)
 
@@ -46,6 +60,7 @@ class AuthorResource(Resource):
         return {"message": "Author updated", "author_id": author.id}, 200
 
     def delete(self, author_id):
+        """Delete an author by ID"""
         author = Author.query.get(author_id)
         if not author:
             return {"message": "Author not found"}, 404
