e8728c69827f84b6b8e1aa33d691ac41bc85912e
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 12:22:09 +0100

    Refactor book_resource.py to use Flask-RESTX and add Namespace for handling book-related API endpoints

--- resources/book_resource.py
+++ resources/book_resource.py
@@ -1,35 +1,50 @@
-from flask_restful import Resource
+from flask_restx import Resource, fields
 from flask import request
 from models.book import Book, db
 from schemas.book_schema import BookSchema
 from marshmallow import ValidationError
 
+# Initialiser le schéma Marshmallow pour Book
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
 
-class BookResource(Resource):
-    def get(self, book_id=None):
-        if book_id:
-            book = Book.query.get(book_id)
-            if not book:
-                return {"message": "Book not found"}, 404
-            return book_schema.dump(book), 200
-        else:
-            books = Book.query.all()
-            return books_schema.dump(books), 200
+# Définir le modèle pour la documentation Swagger avec Flask-RESTX
+book_model = {
+    "title": fields.String(required=True, description="Title of the book"),
+    "description": fields.String(description="Description of the book"),
+    "publication_date": fields.Date(description="Publication date of the book"),
+    "author_id": fields.Integer(required=True, description="Author ID of the book")
+}
+
+class BookListResource(Resource):
+    def get(self):
+        """List all books"""
+        books = Book.query.all()
+        return books_schema.dump(books), 200
 
     def post(self):
+        """Create a new book"""
         try:
             data = book_schema.load(request.get_json())
         except ValidationError as err:
             return {"errors": err.messages}, 400
 
-        new_book = data if isinstance(data, Book) else Book(**data)
+        new_book = Book(**data)
         db.session.add(new_book)
         db.session.commit()
         return {"message": "Book created", "book_id": new_book.id}, 201
 
+
+class BookResource(Resource):
+    def get(self, book_id):
+        """Retrieve a book by ID"""
+        book = Book.query.get(book_id)
+        if not book:
+            return {"message": "Book not found"}, 404
+        return book_schema.dump(book), 200
+
     def put(self, book_id):
+        """Update a book by ID"""
         book = Book.query.get(book_id)
         if not book:
             return {"message": "Book not found"}, 404
@@ -39,6 +54,7 @@ class BookResource(Resource):
         except ValidationError as err:
             return {"errors": err.messages}, 400
 
+        # Mettre à jour les champs du livre existant
         for key, value in data.items():
             setattr(book, key, value)
 
@@ -46,6 +62,7 @@ class BookResource(Resource):
         return {"message": "Book updated", "book_id": book.id}, 200
 
     def delete(self, book_id):
+        """Delete a book by ID"""
         book = Book.query.get(book_id)
         if not book:
             return {"message": "Book not found"}, 404
