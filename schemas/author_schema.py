76a0edfba75fd05affe1e41df66dc8602e31706b
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 11:42:44 +0100

    Add AuthorSchema for marshmallow serialization and deserialization

---
+++ schemas/author_schema.py
@@ -0,0 +1,12 @@
+from marshmallow import fields
+from schemas import ma
+from models.author import Author
+
+class AuthorSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Author
+        load_instance = True
+    
+    name = fields.Str(required=True)
+    biography = fields.Str()
+    birth_date = fields.Date(format='%Y-%m-%d')
