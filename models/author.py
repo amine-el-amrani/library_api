407fb2f618b7e1d4cb12fcacb1b9eea5b98eb66d
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 11:09:40 +0100

    Add Author model with columns for name, biography, and birth date

---
+++ models/author.py
@@ -0,0 +1,11 @@
+from models.book import db
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    biography = db.Column(db.String(255))
+    birth_date = db.Column(db.Date)
+
+
+    books = db.relationship('Book', back_populates='author')
