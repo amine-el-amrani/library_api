d110d4d88619c0e2a8b31b2925737c46d4d70ba1
Amine El amrani <amine_el_amrani@outlook.com>
2024-10-27 11:43:05 +0100

    Add BookResource for handling book-related API endpoints

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask
-from models.book import Book
-from models.author import Author
-from models.book import db
+from flask_restful import Api
+from ressources.book_ressource import BookResource
+from models.db import db
+from schemas.schemas import ma
 from blueprints.books_bp import books_bp
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db.init_app(app)
+ma.init_app(app)
+api = Api(app)
 
 with app.app_context():
     db.create_all()
+
+
+api.add_resource(BookResource, '/books', '/books/<int:book_id>')
+
 
 app.register_blueprint(books_bp, url_prefix='/books')
 
